b00077c6227aaaa085ee0a2c4fda1ef652b2212b
poluchpoker <90266390+poluchpoker@users.noreply.github.com>
2022-10-19 00:03:18 +0300

    Update Number2.java

--- src/Number2.java
+++ src/Number2.java
@@ -20,18 +20,33 @@ public class Number2 {
         int cnt = 0;
         String word = inp.nextLine();
         String output = "";
+        String symbol = "#";
 
         do{
-            if (word.charAt(cnt) == random_word.charAt(cnt)){
-                output += word.charAt(cnt);
-                cnt += 1;
-            }else{
-                String symbol = "#";
-                System.out.println(output + symbol.repeat(15 - cnt));
-                cnt = 0;
+            if (cnt >= word.length()){
+                String word1 = word;
+                System.out.println("Введите слово заново, пока длина его не будет больше предыдущего: ");
                 word = inp.nextLine();
+                while (word.length() <= word1.length()){
+                    System.out.println("Введите слово заново, пока длина его не станет больше предыдущего: ");
+                    word = inp.nextLine();
+                }
+
             }
+            if (word.equals(random_word)) {
+                output = random_word;
+                break;
+            }
+            if (word.charAt(cnt) == random_word.charAt(cnt)){
+                output = output.substring(0, cnt) + random_word.charAt(cnt);
+                cnt += 1;
+                System.out.println(output + symbol.repeat(15 - cnt));
+            }else{
+                System.out.println(output + symbol.repeat(15 - cnt));
+            }
+            System.out.println("Введите слово: ");
+            word = inp.nextLine();
         }while (cnt != random_word.length());
-        System.out.println("Вы угадали слово: " + output);
+        System.out.printf("Вы угадали слово - %s, поздравляю!", output);
     }
 }
